85560deea5d589fe781433e94a8ba52a484bf8b1
Kyle Ranger <zranger2345@gmail.com>
2020-01-27 21:17:28 -0600

    Add hashDB header file

---
+++ hashDB.h
@@ -0,0 +1,22 @@
+#ifndef _HASHDB_HASHDB_H_
+#define _HASHDB_HASHDB_H_
+
+// Represents the database
+struct hashDB {
+	// start of the linked list of active segment files
+	struct segment_file *head;
+};
+
+struct hashDB *hashDB_init(const char *data_dir);
+void hashDB_free(struct hashDB *db);
+int hashDB_put(struct hashDB *db, int key, int val_len, char *val);
+int hashDB_get(struct hashDB *db, int key, char **val);
+int hashDB_delete(struct hashDB *db, int key);
+int hashDB_compact(struct hashDB *db,
+                   struct segment_file *s1,
+                   struct segment_file *s2);
+int hashDB_merge(struct hashDB *db,
+                 struct segment_file *s1,
+                 struct segment_file *s2);
+
+#endif
