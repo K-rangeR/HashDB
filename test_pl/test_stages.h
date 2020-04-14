322b114cdab5cf304691d6fdb41385c2614f1756
Kyle Ranger <zranger2345@gmail.com>
2020-04-14 17:46:49 -0500

    Add stage functions to test_stages.h

---
+++ test_pl/test_stages.h
@@ -0,0 +1,20 @@
+#ifndef _TEST_PIPELINE_TEST_STAGS_H_
+#define _TEST_PIPELINE_TEST_STAGS_H_
+
+#include "stage.h"
+
+int test_segf_get(struct stage *s);
+int test_segf_put(struct stage *s);
+int test_segf_delete(struct stage *s);
+int test_segf_create_file(struct stage *s);
+int test_segf_delete_file(struct stage *s);
+
+int test_hashdb_get(struct stage *s);
+int test_hashdb_put(struct stage *s);
+int test_hashdb_delete(struct stage *s);
+int test_hashdb_repopulate(struct stage *s);
+int test_hashdb_mkempty(struct stage *s);
+int test_hashdb_compact(struct stage *s);
+int test_hashdb_merge(struct stage *s);
+
+#endif
