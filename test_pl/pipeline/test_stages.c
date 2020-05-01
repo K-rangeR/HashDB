9a52747ad47b390df59a3d9f931bad79cdb2e731
Kyle Ranger <zranger2345@gmail.com>
2020-05-01 18:07:26 -0500

    Write test_hashdb_merge

--- test_pl/pipeline/test_stages.c
+++ test_pl/pipeline/test_stages.c
@@ -238,9 +238,45 @@ int test_hashdb_compact(struct stage *s)
 }
 
 
+/*
+ * Tests the hashDB_merge function
+ */
 int test_hashdb_merge(struct stage *s)
 {
-	printf("test_hashdb_merge\n");
+	struct hashDB *tdb = NULL;
+	if ((tdb = hashDB_init(TEST_DATA_DIR)) == NULL) {
+		printf("[F]: test_hashdb_compact: hashDB_init: %s\n", 
+			strerror(errno));
+		return 0;
+	}
+
+	struct segment_file *one = NULL, *two = NULL;
+	struct segment_file *curr = tdb->head;
+	while (curr) {
+		int curr_id = get_id_from_fname(curr->name);
+		if (curr_id == id_one(s)) {
+			one = curr;
+		}
+		if (curr_id == id_two(s)) {
+			two = curr;
+		}
+		curr = curr->next;
+	}
+
+	if (one == NULL || two == NULL) {
+		printf("[!]: test_hashdb_compact: could not find ID\n");
+		hashDB_free(tdb);	
+		return 0;
+	}
+
+	if (hashDB_merge(tdb, one, two) == -1) {
+		printf("[F]: test_hashdb_merge: hashDB_merge: %s\n", 
+			strerror(errno));
+		hashDB_free(tdb);
+		return 0;
+	}
+
+	hashDB_free(tdb);
 	return 1;
 }
 
